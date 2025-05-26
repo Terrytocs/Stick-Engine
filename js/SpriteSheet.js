71ca4b2e5c7c95ec1bd2299eb57e32df8f0259ab
Theodore Scott-Henry <64044647+Terrytocs@users.noreply.github.com>
2025-05-26 10:59:13 -0700

    Update SpriteSheet.js

--- js/SpriteSheet.js
+++ js/SpriteSheet.js
@@ -12,6 +12,27 @@ export default class SpriteSheet{
     buffer.height=destSize.y;
     ctx=buffer.getContext('2d');
     
-    ctx.drawImage
+    ctx.drawImage(
+      this.img,
+      src.x,
+      src.y,
+      srcSize.x,
+      srcSize.y,
+      dest.x,
+      dest.y,
+      destSize.x,
+      destSize.y
+    );
+
+    this.sprite.set(id,buffer);
+  }
+  draw(ctx,id,pos){
+    var img=this.sprite.get(id);
+
+    ctx.drawImage(
+      img,
+      pos.x,
+      pos.y
+    );
   }
 }
