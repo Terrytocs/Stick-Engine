6290b0c5c9d3534f37e8bf47a899a6363f510078
Theodore Scott-Henry <64044647+Terrytocs@users.noreply.github.com>
2025-10-21 22:31:54 -0500

    Delete js directory

--- js/Utils.js
+++
@@ -1,7 +0,0 @@
-('use strict');
-
-export default class Utils{
-  clamp(num,min,max){
-    return Math.max(min,Math.min(max,num));
-  }
-}
--- js/Viewport.js
+++
@@ -1,7 +0,0 @@
-('use strict');
-
-export default class Viewport{
-  constructor(pos,offset){
-    this.pos=pos;
-  } 
-}
